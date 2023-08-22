92ba94b15c26b956f0af1e7307a243bbed787e19
neha93995 <nehabaghelcs190302@gmail.com>
2023-08-22 18:11:56 +0530

    detete redux page

--- src/components/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import UserReducer from './UserReducer';
-
-
-const Store = configureStore({
-    reducer:UserReducer,
-  })
-  export default Store;
